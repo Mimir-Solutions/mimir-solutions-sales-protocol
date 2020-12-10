3dfbd41d5ef4a318254c9997ae2dca3b35d0d163
cyotee doge <cyotee@gmail.com>
2020-12-10 15:35:19 -0800

    Created initial scaffold for sales platform
    Syncing submodules

---
+++ contracts/QPLGMESaleMinter.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.5;
+
+import "hardhat/console.sol";
+
+import "./dependencies/holyzeppelin/contracts/access/Ownable.sol";
+
+contract QPLGMESaleMinter is Ownable {
+
+    
+}
