07e0ff9da8dc55a1c9964dcd7dcd795cb862a211
cyotee doge <cyotee@gmail.com>
2020-12-16 05:24:41 -0800

    Syncing submodules

--- contracts/sales/manager/SalesManager.sol
+++ contracts/sales/manager/SalesManager.sol
@@ -4,5 +4,6 @@ pragma solidity 0.7.5;
 import "hardhat/console.sol";
 
 contract SalesManager {
-    
+    // Try to make a list of conteracts to sales manager. This to be will be used for pulling Sales.sol. An pushing ISalesPlatform.sol.
+
 }
